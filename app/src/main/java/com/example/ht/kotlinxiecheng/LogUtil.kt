e430c75d17a66ea3832e995306d718983b1dacf2
LIENLIN <lienlin@huatu.com>
2020-02-24 18:15:16 +0800

    提交哈希后第一次提交

--- app/src/main/java/com/example/ht/kotlinxiecheng/LogUtil.kt
+++ app/src/main/java/com/example/ht/kotlinxiecheng/LogUtil.kt
@@ -4,6 +4,10 @@ import android.util.Log
 
 object LogUtil {
     public fun showMessage(msg:String){
-        Log.d("LogUtil","打印日志：2222$msg")
+        Log.d("LogUtil","打印日志：3333$msg")
+    }
+
+    fun showMessage2(){
+
     }
 }
